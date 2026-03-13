1728141836ce3a44145c14df730d1ac1474b107e
Sher016 <sherlyn1620@gmail.com>
2026-03-12 20:39:17 -0500

    emotion_detector

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,6 +1,7 @@
 import requests
+import json
 
-def emotion_detector(text_to_analyze):
+def emotion_detector(text_a_analizar):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {
         "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock",
@@ -8,13 +9,35 @@ def emotion_detector(text_to_analyze):
     }
     input_json = {
         "raw_document": {
-            "text": text_to_analyze
+            "text": text_a_analizar
         }
     }
 
     response = requests.post(url, headers=headers, json=input_json)
 
     if response.status_code == 200:
-        return response.json()  # Devuelve el JSON con la predicción de emociones
+    
+        data = response.json()
+        
+        emotions = data.get('predictions', [{}])[0].get('emotions', {})
+        
+        anger_score = emotions.get('anger', 0)
+        disgust_score = emotions.get('disgust', 0)
+        fear_score = emotions.get('fear', 0)
+        joy_score = emotions.get('joy', 0)
+        sadness_score = emotions.get('sadness', 0)
+        
+        emotion_scores = {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score
+        }
+        
+        dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+        emotion_scores['dominant_emotion'] = dominant_emotion
+        
+        return emotion_scores
     else:
         return {"error": f"Request failed with status code {response.status_code}"}
